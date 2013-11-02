77016baf55ee280e42c344b28b12ebfdc0466e80
ZWang <hyopalexus@gmail.com>
2013-11-01 22:10:21 -0700

    just distribute or over and left?

--- project-3.lisp
+++ project-3.lisp
@@ -9,6 +9,15 @@
 (defun isAtomicClause (l)
 	(or (isAtomic l) (isNegativeAtomicClause l))
 	)
+	
+(defun listP_Not (l)
+	(if (isAtomic l)
+		nil
+	(if (isNegativeAtomicClause l)
+		nil
+		(listp l)
+	)))
+
 
 ;;;get rid of implications
 (defun implicationOut (l)
@@ -20,6 +29,18 @@
 	(if (eq 'IF (car l))
 		(cons 'OR (list (list 'NOT (implicationOut (cadr l))) (implicationOut (caddr l))))
 		(cons (car l) (mapcar #'implicationOut (cdr l)))))))
+		
+		
+(defun decomposeIFF (l)
+	(
+	if (null l)
+		nil
+	(if (isAtomicClause l)
+		l
+	(if (eq 'IFF (car l))
+		(cons 'AND (list (list 'IF (decomposeIFF (cadr l)) (decomposeIFF (caddr l))) (list 'IF (decomposeIFF (caddr l)) (decomposeIFF (cadr l)))))
+		(cons (car l) (mapcar #'decomposeIFF (cdr l)))))))
+
 		
 (defun negationIn_single (l)
   "handle negation for a single clause"
@@ -49,10 +70,67 @@
 	(if (eq 'NOT (car l))
 		(negationIn_single (cadr l))
 		(cons (car l) (mapcar #'negationIn (cdr l)))))))
+		
+(defun eliminateIdenticalAndOrOriginal (x l)
+	"eliminate (and (and ...)) and (or (or ...)) cases"
+	(
+	if (isAtomicClause l)
+		l
+	(if (eq x (car l))
+		;;;(mapcar #'(lambda (l) (eliminateIdenticalAndOr x l)) (cdr l))
+		(eliminateIdenticalAndOr x (cdr l))
+		(cons (car l) (mapcar #'(lambda (p) (eliminateIdenticalAndOr (car l) p)) (cdr l))))))
+		
+(defun eliminateIdenticalAndOr1 (x l)
+	"eliminate (and (and ...)) and (or (or ...)) cases"
+	(
+	if(null l)
+		nil
+	(if (isAtomicClause l)
+		l
+	(if (eq x (car l))
+		(eliminateIdenticalAndOr x (cdr l))
+		(cons (car l) (eliminateIdenticalAndOr (car l) (cdr l)))))))
+		
+
+(defun eliminateIdenticalAndOr (x l)
+	"eliminate (and (and ...)) and (or (or ...)) cases"
+	(
+	if(null l)
+		nil
+	(if (isAtomicClause l)
+		l
+	(if (eq x (car l))
+		(eliminateIdenticalAndOr x (cdr l))
+		(if (listP_Not (car l))
+			(append (eliminateIdenticalAndOr x (car l)) (eliminateIdenticalAndOr x (cdr l)))
+			(cons (car l) (eliminateIdenticalAndOr (car l) (cdr l)))
+			)))))
+
+		
+(defun handleIdenticalAndOr (x l)
+	"sweep through the sentence and call appropriate eliminateIdenticalAndOr"
+	(if (null l)
+		nil
+	(if (and (= (length l) 1) (isAtomic (car l)))
+		l
+	(if (isAtomicClause l)
+		l
+	(if (eq x (caar l))
+		(append (eliminateIdenticalAndOr x (car l)) (handleIdenticalAndOr x (cdr l)))
+		(cons (eliminateIdenticalAndOr x (car l)) (handleIdenticalAndOr x (cdr l))))))))
+
+(defun IdenticalAndOr (l)
+	(if (isAtomicClause l)
+		l
+	(cons (car l) (handleIdenticalAndOr (car l) (cdr l)))))
+
 
 
 (defun convert-to-CNF (l)
+	(setf l (decomposeIFF l))
 	(setf l (implicationOut l))
 	(setf l (negationIn l))
+	(setf l (IdenticalAndOr l))
 	l
 )
